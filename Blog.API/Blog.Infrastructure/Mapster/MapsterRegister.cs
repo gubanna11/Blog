a4b349abab42c90f34cb726c47eccdd18590dc2a
Maksull <kyrkyrkyr99@gmail.com>
2023-10-24 21:00:24 +0300

    Add MediatR

---
+++ Blog.API/Blog.Infrastructure/Mapster/MapsterRegister.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Core.Entities;
+using Mapster;
+
+namespace Blog.Infrastructure.Mapster;
+
+public sealed class MapsterRegister : IRegister
+{
+    public void Register(TypeAdapterConfig config)
+    {
+        config.ForType<CreateCategoryRequest, Category>();
+        config.ForType<UpdateCategoryRequest, Category>();
+
+        config.ForType<CreateCommentRequest, Comment>();
+        config.ForType<UpdateCommentRequest, Comment>();
+
+        config.ForType<CreatePostRequest, Post>();
+        config.ForType<UpdatePostRequest, Post>();
+    }
+}
