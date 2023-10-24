2124da5ea81f3549d46f0da457465fff62c63d41
Maksull <49917345+Maksull@users.noreply.github.com>
2023-10-24 21:06:32 +0300

    Add MediatR (#8)

--- Blog.API/Blog.Dependencies/Dependencies.cs
+++ Blog.API/Blog.Dependencies/Dependencies.cs
@@ -1,14 +1,19 @@
 ﻿using Blog.Core.Entities;
 using Blog.Infrastructure.Data;
+using Blog.Infrastructure.Mapster;
+using Mapster;
+using MapsterMapper;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Blog.Dependencies;
+
 public static class Dependencies
 {
-    public static IServiceCollection ConfigureEnvironment(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection ConfigureEnvironment(this IServiceCollection services,
+        IConfiguration configuration)
     {
         services.ConfigureDatabase(configuration);
         return services;
@@ -16,12 +21,20 @@ public static class Dependencies
 
     private static void ConfigureDatabase(this IServiceCollection services, IConfiguration configuration)
     {
-        string connectionString = configuration["ConnectionString:String"]!;
-
-        services.AddDbContext<ApiDataContext>(options => options.UseSqlServer(connectionString));
+        services.AddDbContext<ApiDataContext>(
+            options => options.UseSqlServer(configuration["ConnectionString:String"]!));
 
         services.AddIdentity<User, IdentityRole>()
-                .AddEntityFrameworkStores<ApiDataContext>()
-                .AddDefaultTokenProviders();
+            .AddEntityFrameworkStores<ApiDataContext>()
+            .AddDefaultTokenProviders();
+    }
+
+    private static void ConfigureMapster(this IServiceCollection services)
+    {
+        TypeAdapterConfig config = new();
+        config.Apply(new MapsterRegister());
+        services.AddSingleton(config);
+
+        services.AddSingleton<IMapper>(sp => new ServiceMapper(sp, config));
     }
 }
