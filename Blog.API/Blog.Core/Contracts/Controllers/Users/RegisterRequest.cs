03f8520fce1cbb4f5718e9bfb0dfe7c27a7f0c1a
Maksull <kyrkyrkyr99@gmail.com>
2023-10-24 20:51:53 +0300

    Add Contracts

---
+++ Blog.API/Blog.Core/Contracts/Controllers/Users/RegisterRequest.cs
@@ -0,0 +1,4 @@
+﻿namespace Blog.Core.Contracts.Controllers.Users;
+
+public sealed record RegisterRequest(string FirstName, string LastName, string Username, string Email,
+    string PhoneNumber, string Password, string ConfirmPassword);
