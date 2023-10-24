b31420ca4fd452872bd49260b5fcc5b141278a48
Maksull <49917345+Maksull@users.noreply.github.com>
2023-10-24 21:06:18 +0300

    Add Contracts (#7)

---
+++ Blog.API/Blog.Core/Contracts/Controllers/Comments/CreateCommentRequest.cs
@@ -0,0 +1,5 @@
+﻿using System;
+
+namespace Blog.Core.Contracts.Controllers.Comments;
+
+public sealed record CreateCommentRequest(string Content, Guid PostId, string UserId, DateTime PublishTime, Guid? ParentCommentId);
