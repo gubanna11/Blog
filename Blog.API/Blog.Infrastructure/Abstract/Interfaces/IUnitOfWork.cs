182cf0eb2f2d780d752167b7ee3d9940b436a0da
Hanna Huba <gubanna11.gmail.com>
2023-10-25 11:37:42 +0300

    Added UnitOfWork

---
+++ Blog.API/Blog.Infrastructure/Abstract/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Blog.Infrastructure.Abstract.Interfaces;
+
+public interface IUnitOfWork
+{
+    IGenericRepository<T> GenericRepository<T>() where T : class;
+    Task SaveChangesAsync();
+}
---
+++ Blog.API/Blog.Infrastructure/Abstract/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Blog.Infrastructure.Abstract.Interfaces;
+using Blog.Infrastructure.Data;
+using System.Threading.Tasks;
+
+namespace Blog.Infrastructure.Abstract;
+
+public sealed class UnitOfWork : IUnitOfWork
+{
+    private readonly ApiDataContext _context;
+
+    public UnitOfWork(ApiDataContext context)
+    {
+        _context = context;
+    }
+
+    public IGenericRepository<T> GenericRepository<T>() where T : class
+    {
+        return new GenericRepository<T>(_context);
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
