c5f2c612d305e52448ea4c1cf1c662aab20004db
Maksull <49917345+Maksull@users.noreply.github.com>
2023-10-25 09:41:24 +0300

    Add mock Services. (#10)

--- Blog.API/Blog.Dependencies/Dependencies.cs
+++ Blog.API/Blog.Dependencies/Dependencies.cs
@@ -1,27 +1,40 @@
 ﻿using Blog.Core.Entities;
 using Blog.Infrastructure.Data;
+using Blog.Infrastructure.Services;
+using Blog.Infrastructure.Services.Interfaces;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Blog.Dependencies;
+
 public static class Dependencies
 {
-    public static IServiceCollection ConfigureEnvironment(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection ConfigureEnvironment(this IServiceCollection services,
+        IConfiguration configuration)
     {
         services.ConfigureDatabase(configuration);
+        services.ConfigureServices();
         return services;
     }
 
     private static void ConfigureDatabase(this IServiceCollection services, IConfiguration configuration)
     {
-        string connectionString = configuration["ConnectionString:String"]!;
-
-        services.AddDbContext<ApiDataContext>(options => options.UseSqlServer(connectionString));
+        services.AddDbContext<ApiDataContext>(
+            options => options.UseSqlServer(configuration["ConnectionString:String"]!));
 
         services.AddIdentity<User, IdentityRole>()
-                .AddEntityFrameworkStores<ApiDataContext>()
-                .AddDefaultTokenProviders();
+            .AddEntityFrameworkStores<ApiDataContext>()
+            .AddDefaultTokenProviders();
+    }
+
+    private static void ConfigureServices(this IServiceCollection services)
+    {
+        services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<IPostService, PostService>();
+        services.AddScoped<ICommentService, CommentService>();
+
+        services.AddScoped<IUserService, UserService>();
     }
 }
---
+++ Blog.API/Blog.Infrastructure/Services/PostService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Blog.Core.Entities;
+using Blog.Infrastructure.Services.Interfaces;
+
+namespace Blog.Infrastructure.Services;
+
+//TODO:Implement methods
+public sealed class PostService : IPostService
+{
+    public IEnumerable<Post> GetPosts()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Post?> GetPostById(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Post> CreatePost(Post createPost)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Post?> UpdatePost(Post updatePost)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Post?> DeletePost(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
