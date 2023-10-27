fdd6c72fb5de54344f3cb4e4d9743c400791e9d5
Maksull <kyrkyrkyr99@gmail.com>
2023-10-27 09:47:34 +0300

    Add basic Controllers

---
+++ Blog.API/Blog.API/Controllers/CommentsController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Blog.Core.Contracts.Controllers.Comments;
+using Blog.Core.Entities;
+using Blog.Core.MediatR.Commands.Comments;
+using Blog.Core.MediatR.Queries.Comments;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public sealed class CommentsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CommentsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Comment>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(NotFoundResult))]
+    public async Task<IActionResult> GetComments(CancellationToken cancellationToken)
+    {
+        var comments = await _mediator.Send(new GetCommentsQuery(), cancellationToken);
+
+        if (comments.Any()) return Ok(comments);
+
+        return NotFound();
+    }
+
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Comment))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(NotFoundResult))]
+    public async Task<IActionResult> GetCommentById([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var comment = await _mediator.Send(new GetCommentByIdQuery(id), cancellationToken);
+
+        if (comment is not null) return Ok(comment);
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Comment))]
+    public async Task<IActionResult> CreateComment([FromBody] CreateCommentRequest createComment,
+        CancellationToken cancellationToken)
+    {
+        var comment = await _mediator.Send(new CreateCommentCommand(createComment), cancellationToken);
+
+        return Ok(comment);
+    }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Comment))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(NotFoundResult))]
+    public async Task<IActionResult> UpdateComment([FromBody] UpdateCommentRequest updateComment,
+        CancellationToken cancellationToken)
+    {
+        var comment = await _mediator.Send(new UpdateCommentCommand(updateComment), cancellationToken);
+
+        if (comment is not null) return Ok(comment);
+
+        return NotFound();
+    }
+
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Comment))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(NotFoundResult))]
+    public async Task<IActionResult> DeleteComment([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var comment = await _mediator.Send(new DeleteCommentCommand(id), cancellationToken);
+
+        if (comment is not null) return Ok(comment);
+
+        return NotFound();
+    }
+}
---
+++ Blog.API/Blog.API/Controllers/UserController.cs
@@ -0,0 +1,63 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.API.Controllers;
+
+//TODO: Consider endpoints and implement them
+[Route("api/[controller]")]
+[ApiController]
+public sealed class UserController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public UserController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /*[HttpPost("login")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(JwtResponse))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(BadRequestObjectResult))]
+    public async Task<IActionResult> Login([FromBody] LoginRequest request)
+    {
+        var result = await _mediator.Send(new LoginCommand(request));
+
+        if (result is not null)
+        {
+
+            return Ok(result);
+        }
+
+        return BadRequest("Invalid credentials");
+    }*/
+
+    /*[HttpPost("register")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OkResult))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(BadRequestObjectResult))]
+    public async Task<IActionResult> Register([FromBody] RegisterRequest request)
+    {
+        var result = await _mediator.Send(new RegisterCommand(request));
+
+        if (!result.Any())
+        {
+            return Ok();
+        }
+
+        return BadRequest(new RegisterFailed(result));
+    }*/
+
+    /*[HttpGet("confirmEmail")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(OkResult))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(NotFoundResult))]
+    public async Task<IActionResult> ConfirmEmail([FromQuery] string userId, [FromQuery] string token)
+    {
+        var result = await _mediator.Send(new ConfirmEmailCommand(userId, token));
+
+        if (result)
+        {
+            return Ok();
+        }
+
+        return NotFound();
+    }*/
+}
