2ce79adababf72bc2f774700f1c6fe7a20b05545
Maksull <49917345+Maksull@users.noreply.github.com>
2023-10-29 16:06:45 +0200

    Controllers (#15)
    * Add basic Controllers
    
    * Fix ConnectionString
    
    * Fix connection string

---
+++ Blog.API/Blog.API/Controllers/CategoriesController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Blog.Core.Contracts.Controllers.Categories;
+using Blog.Core.Entities;
+using Blog.Core.MediatR.Commands.Categories;
+using Blog.Core.MediatR.Queries.Categories;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public sealed class CategoriesController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public CategoriesController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Category>))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(NotFoundResult))]
+    public async Task<IActionResult> GetCategories(CancellationToken cancellationToken)
+    {
+        var categories = await _mediator.Send(new GetCategoriesQuery(), cancellationToken);
+
+        if (categories.Any()) return Ok(categories);
+
+        return NotFound();
+    }
+
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Category))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(NotFoundResult))]
+    public async Task<IActionResult> GetCategoryById([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var category = await _mediator.Send(new GetCategoryByIdQuery(id), cancellationToken);
+
+        if (category is not null) return Ok(category);
+
+        return NotFound();
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Category))]
+    public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryRequest createCategory,
+        CancellationToken cancellationToken)
+    {
+        var category = await _mediator.Send(new CreateCategoryCommand(createCategory), cancellationToken);
+
+        return Ok(category);
+    }
+
+    [HttpPut]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Category))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(NotFoundResult))]
+    public async Task<IActionResult> UpdateCategory([FromBody] UpdateCategoryRequest updateCategory,
+        CancellationToken cancellationToken)
+    {
+        var category = await _mediator.Send(new UpdateCategoryCommand(updateCategory), cancellationToken);
+
+        if (category is not null) return Ok(category);
+
+        return NotFound();
+    }
+
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Category))]
+    [ProducesResponseType(StatusCodes.Status404NotFound, Type = typeof(NotFoundResult))]
+    public async Task<IActionResult> DeleteCategory([FromRoute] Guid id, CancellationToken cancellationToken)
+    {
+        var category = await _mediator.Send(new DeleteCategoryCommand(id), cancellationToken);
+
+        if (category is not null) return Ok(category);
+
+        return NotFound();
+    }
+}
