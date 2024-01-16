a3495891a8edbf598742938bf672d518b7ee694b
Maksull <49917345+Maksull@users.noreply.github.com>
2024-01-16 07:22:11 +0200

    Pagination (#21)
    * Start implementing pagination
    
    * Cursor pagination beginning
    
    * Update return type for offset paged categories
    
    * Update categories cursor pagination. Now it returns CategoryResponse
    
    * Fix PagedResponse.cs and CursorPagedResponse.cs
    
    * Add optional include to paged categories
    
    * Extract paged parameters into separate classes
    
    * Implement pagination for all entities

--- Blog.API/Blog.Core/Entities/Comment.cs
+++ Blog.API/Blog.Core/Entities/Comment.cs
@@ -19,6 +19,7 @@ public sealed class Comment
     [ForeignKey(nameof(PostId))]
     [ProtoMember(3)]
     public Guid PostId { get; set; }
+    public Post? Post { get; set; }
 
     [ForeignKey(nameof(UserId))]
     [ProtoMember(4)]
--- Blog.API/Blog.Infrastructure/Services/Interfaces/IPostService.cs
+++ Blog.API/Blog.Infrastructure/Services/Interfaces/IPostService.cs
@@ -1,13 +1,18 @@
 ﻿using Blog.Core.Contracts.Controllers.Posts;
 using System;
 using System.Collections.Generic;
+using System.Threading;
 using System.Threading.Tasks;
+using Blog.Core.Contracts.Controllers;
+using Blog.Core.MediatR.Queries.Posts;
 
 namespace Blog.Infrastructure.Services.Interfaces;
 
 public interface IPostService
 {
     Task<IEnumerable<PostResponse>> GetPosts();
+    Task<PagedResponse<PostResponse>> GetPagedPosts(GetPagedPostsQuery request, CancellationToken cancellationToken);
+    Task<CursorPagedResponse<PostResponse>> GetCursorPagedPosts(GetCursorPagedPostsQuery request, CancellationToken cancellationToken);
     Task<PostResponse?> GetPostById(Guid id);
     Task<PostResponse?> CreatePost(CreatePostRequest createPost);
     Task<PostResponse?> UpdatePost(UpdatePostRequest updatePost);
